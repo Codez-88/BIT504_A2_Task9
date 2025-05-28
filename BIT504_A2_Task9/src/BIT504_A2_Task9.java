666c20d96d8d39cacfdad3142e6f84830d264273
Codez-88 <cody.knight@hotmail.com>
2025-05-28 22:52:43 +1200

    This is my final change commit.

--- BIT504_A2_Task9/src/BIT504_A2_Task9.java
+++ BIT504_A2_Task9/src/BIT504_A2_Task9.java
@@ -4,6 +4,7 @@ public class BIT504_A2_Task9 {
     public static void main(String[] args) {
         System.out.println("Hello, this is Task 9 for BIT504.");
         System.out.println("This extra text is for my second commit!");
+        System.out.println("This extra text is for my final commit!");
 	}
 
 }
